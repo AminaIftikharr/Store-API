c4d5e45f5bf0d4e4b33596c0d720eb7b5393a93a
Amina Iftikhar <144228946+AminaIftikharr@users.noreply.github.com>
2024-01-30 23:58:37 +0500

    connected to databse

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const app = express();
+app.use(bodyParser.json());
+require('dotenv').config();
+const PORT = 5000;
+require('./db');
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
